676387ad003555d34b751974db9e207db8bec25f
Ariyan Kashyap <100826289+VeNoM000001@users.noreply.github.com>
2023-02-23 18:01:13 +0530

    Update AllAnime.js

--- src/components/allAnime/AllAnime.js
+++ src/components/allAnime/AllAnime.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./AllAnime.scss";
 import { useDispatch } from "react-redux";
 import { listReducer } from "../../redux/yourList/ListSlice";
