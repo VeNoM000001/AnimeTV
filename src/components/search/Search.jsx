5709bc885ecb02a9c187a737f08d9651c9192bc2
Ariyan Kashyap <100826289+VeNoM000001@users.noreply.github.com>
2023-02-23 18:01:59 +0530

    Update Search.jsx

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useRef } from "react";
 import "./Search.scss";
 import { FaSearch } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
